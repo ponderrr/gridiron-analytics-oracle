9cc23cf08ad36426c76d5e16773c228c97eafd51
ponder <robert.ponder@selu.edu>
2025-07-02 21:31:18 -0500

    fix: bugs from recent commit

--- src/components/ui/common/index.ts
+++ src/components/ui/common/index.ts
@@ -1,6 +1,4 @@
-export { default as SearchFilters } from "./SearchFilters";
 export { default as StatGrid } from "./StatGrid";
 export { default as LoadingState } from "./LoadingState";
 
-export type { SearchFiltersProps, FilterOption } from "./SearchFilters";
 export type { StatGridProps, StatData } from "./StatGrid";
