8fd80e4d8eb6d499099e74df25f7772249095ae0
ponder <robert.ponder@selu.edu>
2025-07-10 13:18:53 -0500

    fix: import for weekly refresh function

--- supabase/functions/weekly_refresh/index.ts
+++ supabase/functions/weekly_refresh/index.ts
@@ -1,5 +1,5 @@
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { createHash } from "https://deno.land/std@0.168.0/hash/mod.ts";
+import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
+import { createHash } from "https://deno.land/std@0.224.0/crypto/mod.ts";
 import {
   ETLBase,
   ETLRun,
